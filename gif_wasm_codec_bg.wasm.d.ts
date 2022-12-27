1334e8700f4a568545f9785f5528b4ee5080ae8c
RuyiLi <ruyili2002@gmail.com>
2022-12-27 21:53:59 +0000

    Update pkg to output generated at 2dea65a

---
+++ gif_wasm_codec_bg.wasm.d.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_decodedgif_free(a: number): void;
+export function __wbg_get_decodedgif_num_frames(a: number): number;
+export function __wbg_set_decodedgif_num_frames(a: number, b: number): void;
+export function decodedgif_frames(a: number): number;
+export function decodedgif_metadata(a: number): number;
+export function decode(a: number, b: number): number;
+export function __wbg_encoder_free(a: number): void;
+export function __wbg_get_encoder_width(a: number): number;
+export function __wbg_set_encoder_width(a: number, b: number): void;
+export function __wbg_get_encoder_height(a: number): number;
+export function __wbg_set_encoder_height(a: number, b: number): void;
+export function encoder_new(a: number, b: number): number;
+export function encoder_add_frame(a: number, b: number, c: number): void;
+export function encoder_finish(a: number): number;
+export function __wbindgen_malloc(a: number): number;
